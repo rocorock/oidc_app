019075878187ddbe8853771a55ce0c14eb66249b
rocorock <t.o94th@gmail.com>
2021-02-15 00:10:42 +0900

    create mock ver

--- application/src/main/java/rocorock/oidc_app/view/HelloController.java
+++ application/src/main/java/rocorock/oidc_app/view/HelloController.java
@@ -6,9 +6,33 @@ import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
 public class HelloController{
-    @RequestMapping("/layout")
-    public String hello(Model model){
-        model.addAttribute("hello", "Hello World!");
-        return "layout/media";
+    @RequestMapping("/home")
+    public String home(Model model){
+        return "home";
+    }
+
+    @RequestMapping("/menu")
+    public String menu(Model model){
+        return "menu";
+    }
+
+    @RequestMapping("/access")
+    public String access(Model model){
+        return "access";
+    }
+
+    @RequestMapping("/recruit")
+    public String recruit(Model model){
+        return "recruit";
+    }
+
+    @RequestMapping("/manual")
+    public String manual(Model model){
+        return "manual";
+    }
+
+    @RequestMapping("/mypage")
+    public String mypage(Model model){
+        return "mypage";
     }
 }
